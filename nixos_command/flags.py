3912198dd8949d6dae0a09b37c77221d50c18bd3
Max <max@privatevoid.net>
2022-02-23 20:28:51 +0100

    --profile-name is not a path

--- nixos_command/flags.py
+++ nixos_command/flags.py
@@ -7,7 +7,7 @@ def withInstallable(f):
 
 
 def withProfile(f):
-    f = click.option("-p","--profile-name", help="Profile in which NixOS will be installed", type=click.Path())(f)
+    f = click.option("-p","--profile-name", help="Profile in which NixOS will be installed")(f)
     return f
 
 
